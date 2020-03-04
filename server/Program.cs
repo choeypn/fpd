1f972f128b80f1ef049cdb1cd161b6a77ddd68da
Matthew Le Nguyen <nguye236@wwu.edu>
2020-03-03 19:08:58 -0800

    Read through server documentation with Elliott and made a very basic server that sends across a string converted into to bytes then receives a string from client

---
+++ server/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 9999);
+            server.Start();
+            while(true){
+                TcpClient client = server.AcceptTcpClient();
+                
+                NetworkStream ns = client.GetStream();
+                
+                byte[] test = new byte[100];
+                test = Encoding.Default.GetBytes("test 1 2 3");
+
+                ns.Write(test,0,test.Length);
+
+                while(client.Connected){
+                    byte[] ctest = new byte[1024];
+                    ns.Read(ctest, 0, ctest.Length);
+                    Console.WriteLine(Encoding.Default.GetString(ctest));
+            }
+        }
+    }
+}
